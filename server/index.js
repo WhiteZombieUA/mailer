06ff383a55235c5c6740301effefb97b8878e7f0
Mykola Shvets <inetwz@gmail.com>
2017-12-28 18:04:46 +0200

    added /mails router
    input: name, surname, company
    output: array of emails

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const dns = require('dns');
+const async = require('async');
+
 
 // Configuration
 const app = express();
@@ -13,6 +16,46 @@ app.use(
 );
 
 // Routes
+app.post('/mails', function(req, res) {
+    var name = req.body.name;
+    var surname = req.body.surname;
+    var company = req.body.company;
+    var zones = ['ru', 'com','ua','com.ua','org'];
+
+    var mailNames = [];
+    mailNames[0] = name;
+    mailNames[1] = surname;
+    mailNames[2] = name.charAt(0) + surname;
+    mailNames[3] = name.charAt(0) + '.' + surname;
+    mailNames[4] = name + '.' + surname;
+    mailNames[5] = name + surname;
+
+
+    var domains = zones.map(function (zone) {
+        return company + '.' + zone;
+    });
+
+    async.reduce(domains, [], function(memo, domain, mxDomains) {
+        dns.resolveMx(domain, function(err, addresser) {
+            if (!err) {
+                mxDomains(null, memo.concat(domain));
+            } else {
+                mxDomains(null, memo);
+            }
+        });
+    }, function(err, result) {
+
+        var emails = [];
+        for(var i=0; i<result.length; i++) {
+            for (var j=0; j<mailNames.length; j++) {
+                emails.push(mailNames[j] + '@' + result[i]);
+            }
+        }
+
+        res.send(JSON.stringify({emails: emails}));
+    });
+
+});
 
 // launch app
 app.listen(app.get('port'), () => {
