aadc2f6019ca0fc682e5e09593e8856fb2cbd240
Mykola Shvets <inetwz@gmail.com>
2017-12-31 16:31:24 +0200

    added http request to server
    client sends: name, surname, company
    get from server: array of emails

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+
+  constructor(private http: HttpClient){}
+
+  name: string = '';
+  surname: string = '';
+  company: string = '';
+  emails: any[];
+
+  onNameKeyUP(event: any) {
+    this.name = event.target.value;
+  }
+  onSurnameKeyUP(event: any) {
+    this.surname = event.target.value;
+  }
+  onCompanyKeyUP(event: any) {
+    this.company = event.target.value;
+  }
+
+  getEmails() {
+    let url: string = 'http://localhost:5000/mails?name='+this.name+'&surname='+this.surname+'&company='+this.company;
+    console.log(url);
+
+    this.http.get(url)
+      .subscribe(data => {
+        this.emails = data.emails;
+    });
+  }
+
 }
